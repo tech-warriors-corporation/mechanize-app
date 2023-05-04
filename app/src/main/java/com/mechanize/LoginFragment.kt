976b75db9fdd9bbc9027cc25e743ef7b9a0f9a02
hotequil <hotequil@protonmail.com>
2023-05-04 19:32:42 -0300

    feat: changed some files name and configs

--- app/src/main/java/com/mechanize/LoginFragment.kt
+++ app/src/main/java/com/mechanize/LoginFragment.kt
@@ -6,34 +6,27 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.mechanize.databinding.FragmentSecondBinding
+import com.mechanize.databinding.FragmentLoginBinding
 
-/**
- * A simple [Fragment] subclass as the second destination in the navigation.
- */
-class SecondFragment : Fragment() {
-
-    private var _binding: FragmentSecondBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
-        _binding = FragmentSecondBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonSecond.setOnClickListener {
-            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        binding.homeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_HomeFragment)
         }
     }
 
